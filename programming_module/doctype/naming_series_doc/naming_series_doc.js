6bd3b51f762ce821157d69a77da9211dfd671ab9
gayoosoftware <gayoosoftware@gmail.com>
2024-08-11 08:02:20 +0530

    first commit

---
+++ programming_module/doctype/naming_series_doc/naming_series_doc.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, gs and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('naming series doc', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ programming_module/doctype/submitable_doctype/submitable_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, gs and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('submitable DocType', {
+	// refresh: function(frm) {
+
+	// }
+});
